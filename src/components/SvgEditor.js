4e17a964e1576ecc1e2eb0771b7f49995e331c84
Tuan Ngo <tuan@trexanhlab.com>
2017-03-01 15:39:15 +0700

    style toolbar

--- src/components/SvgEditor.js
+++ src/components/SvgEditor.js
@@ -33,19 +33,19 @@ class SvgEditor extends Component {
     const sampleContent = [
       {
         type: 'rect',
-        x: 40, y: 40, width: 30, height: 30, stroke: "blue", strokeWidth: "1"
+        x: 40, y: 40, width: 100, height: 100, stroke: "blue", strokeWidth: "1"
       },
       {
         type: 'text',
-        x: 100, y: 100, 
+        x: 200, y: 150, 
         'text': 'sample text'
       },
       {
         type: 'image',
         width: 100, 
         height: 100,
-        x: 50,
-        y: 150,
+        x: 60,
+        y: 250,
         'xlink:href': imageSrc  
       }
     ]
@@ -60,11 +60,19 @@ class SvgEditor extends Component {
 
   render () {
     return (
-      <div>
-        <button onClick={this.handleAddRect}>Add rect</button><br/>
-        <button onClick={this.handleAddImage}>Add Image</button><br/>
-        <button onClick={this.handleAddText}>Add Text</button><br/>
-        <Svg ref={node => this.dndSvg = node}/>
+      <div className="container editor">
+        <div className="row">
+          <div className="col-sm-12 col-md-3 toolbar">
+            <div className="list-group">
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleAddImage}><i className="fa fa-picture-o" aria-hidden="true"></i>Add image</a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleAddText}><i className="fa fa fa-font" aria-hidden="true"></i>Add text</a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleAddRect}><i className="fa fa-square-o" aria-hidden="true"></i>Add rect</a>
+            </div>            
+          </div>
+          <div className="col-sm-12 col-md-9">
+            <Svg ref={node => this.dndSvg = node}/>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SvgEditor from './components/SvgEditor';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 ReactDOM.render(
   <SvgEditor />,
